57ff8db0e0dceab4e7d1c7281d5e5b6784cfe6db
Damien Altenburg <daltenburg@rrc.ca>
2025-01-31 12:05:02 -0600

    Added __str__ function and completed documentation.

--- course/lab_course.py
+++ course/lab_course.py
@@ -1,44 +1,83 @@
+"""This module defines the LabCourse class."""
+
+__author__ = "Damien Altenburg"
+__version__ = "1.1.2025"
+
 from course.course import Course
 from department.department import Department
 from student.student import Student
 
 class LabCourse(Course):
-    """TODO"""
+    """Represents a course delivered in a lab."""
 
     def __init__(self, name: str, department: Department, credit_hours: int,
                  capacity: int, current_enrollment: int, lab_equipment: str):
-        """TODO"""
+        """Initializes a new instance of the LabCourse class.
 
-        # Invoke superclass constructor
-        super().__init__(name, department, credit_hours, 
-                         capacity, current_enrollment)
+        Args:
+            name (str): The name of the course.
+            department (Department): The department the course is 
+                delivered.
+            credit_hours (int): The number of credit hours.
+            capacity (int): The number of students that may enroll in 
+                the course.
+            current_enrollment (int): The number of students currently 
+                in the course.
+            lab_equipment (str): The lab equipment required for the 
+                course.
+
+        Raises:
+            ValueError: Raised when the name argument value contains no 
+                non-whitespace characters, the department argument value 
+                is not a Department type, or the credit_hours, 
+                capacity, or current_enrollment argument value is not an
+                integer type.
+        """
         
+        super().__init__(name, department, credit_hours, capacity, current_enrollment)
+
         lab_equipment = lab_equipment.strip()
 
         if len(lab_equipment) == 0:
             lab_equipment = "None"
-        
+
         self.__lab_equipment = lab_equipment
 
     def enroll_student(self, student: Student) -> str:
-        """Adds a student to the course if there is capacity.
-        
+        """Enrolls a student into this course.
+
         Args:
-            student (Student): The student to be enrolled.
-            
+            student (Student): The student to be enrolled into this 
+                course.
+
         Returns:
-            The status of the student's enrollment in the course.
+            str: Returns a confirmation message indicating the 
+                enrollment status.
         """
+        
+        confirmation_message = (
+            f"{student.name} has NOT been enrolled in lab: "
+            f"{self.name} due to insufficient capacity."
+        )
 
-        allowable_enrollment = int(self._capacity / 2)
-
-        if self._current_enrollment < allowable_enrollment:
+        if self._current_enrollment < int(self._capacity / 2):
             self._current_enrollment += 1
 
-            status = (f"{student.name} has been successfully "
-                      f"enrolled in {self.name}.")
+            confirmation_message = \
+                f"{student.name} has been successfully enrolled in {self.name}."
         else:
-            status = (f"{student.name} has NOT been enrolled in lab: "
-                      f"{self.name} due to insufficient capacity.")
+            confirmation_message = (f"{student.name} has NOT been enrolled in lab: "
+                                    f"{self.name} due to insufficient capacity.")
 
-        return status
+        return confirmation_message
+
+    def __str__(self)-> str:
+        """Returns the "informal" or nicely printable string 
+        representation of the object.
+
+        Returns:
+            str: The "informal" or nicely printable string 
+                representation of the object.
+        """
+        
+        return super().__str__() + f"\nLab Equipment: {self.__lab_equipment}"
