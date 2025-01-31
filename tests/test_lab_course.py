3738b65f8e84a262c08324ef917ce85ee9877e3b
Damien Altenburg <daltenburg@rrc.ca>
2025-01-31 12:06:44 -0600

    Added unit tests.

--- tests/test_lab_course.py
+++ tests/test_lab_course.py
@@ -1,10 +1,118 @@
-"""The module defines the TestLabCourse class.
+"""This module defines the TestLabCourse class.
 
-Example:
+Usage: 
+    To execute all tests in the terminal execute the following command:
+    
     $ python -m unittest tests/test_lab_course.py
 """
 
-__author__ = "COMP-2327 Faculty"
-__version__ = "1.1.2025"
-
 import unittest
+from course.lab_course import LabCourse
+from department.department import Department
+from student.student import Student
+
+class TestLabCourse(unittest.TestCase):
+    """Tests the LabCourse class."""
+
+    def setUp(self) -> None:
+        self.name = "ISD"
+        self.department = Department.COMPUTER_SCIENCE
+        self.credit_hours = 90
+        self.capacity = 30
+        self.current_enrollment = 14
+        self.lab_equipment = "Laptop"
+
+        self.course = LabCourse(
+            self.name,
+            self.department,
+            self.credit_hours,
+            self.capacity,
+            self.current_enrollment,
+            self.lab_equipment
+        )
+
+    def test_init_with_lab_equipment_initialize_state(self):
+        # Assert
+        # Superclass private attribute
+        self.assertEqual(self.name, self.course._Course__name)
+        self.assertEqual(self.department, self.course._Course__department)
+        self.assertEqual(self.credit_hours, self.course._Course__credit_hours)
+
+        # Superclass protected attribute
+        self.assertEqual(self.capacity, self.course._capacity)
+        self.assertEqual(self.current_enrollment, self.course._current_enrollment)
+
+        # Subclass private attribute
+        self.assertEqual(self.lab_equipment, self.course._LabCourse__lab_equipment)
+
+    def test_init_with_no_lab_equipment_initialize_state(self):
+        # Arrange
+        self.lab_equipment = ""
+
+        # Act
+        course = LabCourse(
+            self.name,
+            self.department,
+            self.credit_hours,
+            self.capacity,
+            self.current_enrollment,
+            self.lab_equipment
+        )
+
+        # Assert
+        # Superclass private attribute
+        self.assertEqual(self.name, course._Course__name)
+        self.assertEqual(self.department, course._Course__department)
+        self.assertEqual(self.credit_hours, course._Course__credit_hours)
+
+        # Superclass protected attribute
+        self.assertEqual(self.capacity, self.course._capacity)
+        self.assertEqual(self.current_enrollment, course._current_enrollment)
+
+        # Subclass private attribute
+        self.assertEqual("None", course._LabCourse__lab_equipment)
+
+    def test_str_returns_string_representation(self):
+        # Act
+        actual = str(self.course)
+
+        # Assert
+        expected = ("Course: ISD\n"
+                    "Department: Computer Science\n"
+                    "Credit Hours: 90\n"
+                    "Lab Equipment: Laptop")
+
+        self.assertEqual(expected, actual)
+
+    def test_enroll_student_sufficient_capacity(self):
+        # Arrange
+        student = Student(123, "Joe Smith", Department.COMPUTER_SCIENCE)
+
+        # Act
+        actual = self.course.enroll_student(student)
+
+        # Assert
+        expected = "Joe Smith has been successfully enrolled in ISD."
+        self.assertEqual(expected, actual)
+
+    def test_enroll_student_insufficient_capacity(self):
+        # Arrange
+        self.current_enrollment = 15
+
+        course = LabCourse(self.name,
+                           self.department,
+                           self.credit_hours,
+                           self.capacity,
+                           self.current_enrollment,
+                           self.lab_equipment)
+
+        student = Student(123, "Joe Smith", Department.COMPUTER_SCIENCE)
+
+        # Act
+        actual = course.enroll_student(student)
+
+        # Assert
+        expected = ("Joe Smith has NOT been enrolled in lab: ISD due "
+                    "to insufficient capacity.")
+        
+        self.assertEqual(expected, actual)
--- tests/test_lecture_course.py
+++ tests/test_lecture_course.py
@@ -1,10 +1,107 @@
-"""The module defines the TestLectureCourse class.
+"""This module defines the TestLectureCourse class.
 
-Example:
+Usage: 
+    To execute all tests in the terminal execute the following command:
+    
     $ python -m unittest tests/test_lecture_course.py
 """
 
-__author__ = "COMP-2327 Faculty"
-__version__ = "1.1.2025"
-
 import unittest
+from course.lecture_course import LectureCourse
+from department.department import Department
+from student.student import Student
+
+class TestLectureCourse(unittest.TestCase):
+    """Tests the LectureCourse class."""
+
+    def setUp(self) -> None:
+        self.name = "ISD"
+        self.department = Department.COMPUTER_SCIENCE
+        self.credit_hours = 90
+        self.capacity = 30
+        self.current_enrollment = 28
+        self.lecture_hall = "P107"
+
+        self.course = LectureCourse(self.name,
+                                    self.department,
+                                    self.credit_hours,
+                                    self.capacity,
+                                    self.current_enrollment,
+                                    self.lecture_hall)
+
+    def test_init_initialize_state(self):
+        # Assert
+        # Superclass private attribute
+        self.assertEqual(self.name, self.course._Course__name)
+        self.assertEqual(self.department, self.course._Course__department)
+        self.assertEqual(self.credit_hours, self.course._Course__credit_hours)
+
+        # Superclass protected attribute
+        self.assertEqual(self.capacity, self.course._capacity)
+        self.assertEqual(self.current_enrollment, self.course._current_enrollment)
+
+        # Subclass private attribute
+        self.assertEqual(self.lecture_hall, self.course._LectureCourse__lecture_hall)
+
+    def test_init_lecture_hall_is_blank_raises_exception(self):
+        # Arrange
+        self.lecture_hall = ""
+
+        # Act
+        with self.assertRaises(ValueError) as context:
+            course = LectureCourse(
+                self.name,
+                self.department,
+                self.credit_hours,
+                self.capacity,
+                self.current_enrollment,
+                self.lecture_hall
+            )
+
+        # Assert
+        self.assertEqual("Lecture Hall cannot be blank.", str(context.exception))
+
+    def test_str_returns_string_representation(self):
+        # Act
+        actual = str(self.course)
+
+        # Assert
+        expected = ("Course: ISD\n"
+                    "Department: Computer Science\n"
+                    "Credit Hours: 90\n"
+                    "Lecture Hall: P107")
+
+        self.assertEqual(expected, actual)
+
+    def test_enroll_student_sufficient_capacity(self):
+        # Arrange
+        student = Student(123, "Joe Smith", Department.COMPUTER_SCIENCE)
+
+        # Act
+        actual = self.course.enroll_student(student)
+
+        # Assert
+        expected = "Joe Smith has been successfully enrolled in ISD."
+        self.assertEqual(expected, actual)
+
+    def test_enroll_student_insufficient_capacity(self):
+        # Arrange
+        self.current_enrollment = 31
+
+        course = LectureCourse(self.name,
+                               self.department,
+                               self.credit_hours,
+                               self.capacity,
+                               self.current_enrollment,
+                               self.lecture_hall)
+
+        student = Student(123, "Joe Smith", Department.COMPUTER_SCIENCE)
+
+        # Act
+        actual = course.enroll_student(student)
+
+        # Assert
+        expected = ("Joe Smith has NOT been enrolled in lecture: ISD "
+                   "due to insufficient capacity.")
+        
+        self.assertEqual(expected, actual)
