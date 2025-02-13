f9f4cdb9a3efef0eea0d990924014f35638dc60c
Damien Altenburg <daltenburg@rrc.ca>
2025-02-13 12:48:19 -0600

    Added to demonstrate abstract classes and methods.

---
+++ java/Program.java
@@ -0,0 +1,13 @@
+public class Program
+{
+    public static void main(String[] args)
+    {
+        Weapon weapon;
+
+        //weapon = new Weapon();
+
+        weapon = new Sword();
+
+        System.out.println(weapon);
+    }
+}
---
+++ java/Sword.java
@@ -0,0 +1,7 @@
+public class Sword extends Weapon
+{
+    public void attack()
+    {
+        
+    }
+}
